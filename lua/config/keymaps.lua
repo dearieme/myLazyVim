1ec4de761ecad025219e2040f25b4538feb429c0
dearieme <scott@dearie.me.uk>
2023-02-10 12:08:35 +0000

    Add a command + keymap to open markdeep file for current branch

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,15 @@
 -- Default keymaps that are always set:
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Add command to open a 'MarkDeep' notes file in my NOTES directory, named
+-- after the current branch. Requires lualine to get the branch name
+vim.api.nvim_create_user_command("OpenNotes", function()
+  local branch = require("lualine.components.branch.git_branch").get_branch()
+  if branch then
+    local filename = "~/dev/NOTES/tickets/" .. branch .. ".md.html"
+    vim.cmd("e " .. filename)
+  end
+end, {})
+
+vim.keymap.set("n", "<leader>cn", vim.cmd.OpenNotes, { desc = "Open notes file for current branch" })
