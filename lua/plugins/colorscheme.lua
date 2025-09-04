b6bc04ebc3f4f8fdfa0e8e4c2d72989867b7dd95
dearieme <scott@dearie.me.uk>
2025-09-04 10:18:32 +0100

    tmp fix for bufferline integration

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,16 @@
 return {
   { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-mocha" } },
+  --
+  -- Temp fix for catppuccin bufferline integration
+  {
+    "catppuccin/nvim",
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
+  },
 }
+
